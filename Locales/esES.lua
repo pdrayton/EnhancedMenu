ed30ead6549c3c5430dfd5158454c308c6bcedb7
Peter Drayton <pdrayton@gmail.com>
2017-02-20 22:48:50 -0800

    Fixes for LFG context menu

--- Locales/esES.lua
+++ Locales/esES.lua
@@ -7,5 +7,5 @@ select( 2, ... ).L = setmetatable({
     COPY_NAME = "Copiar Nombre",
     SEND_WHO = "¿Quién?",
     ARMORY_URL = "Armería",
-    PUGBOT_COMMAND = "Pugbot",
+    PUGBOT_COMMAND = "PugBot",
 }, { __index = select(2, ... ).L})
--- Locales/zhCN.lua
+++ Locales/zhCN.lua
@@ -6,5 +6,5 @@ select( 2, ... ).L = setmetatable({
 	COPY_NAME = "复制名字",
 	SEND_WHO = "查询",
 	ARMORY_URL = "英雄榜",
-	PUGBOT_COMMAND = "Pugbot",
+	PUGBOT_COMMAND = "PugBot",
 }, { __index = select(2, ... ).L})
